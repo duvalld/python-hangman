752652cba9d45273541c5efe4fe2f9ec4b1f7d19
Duvall Danganan <duvalldanganan@gmail.com>
2023-11-13 22:14:43 +0800

    remove spaces, adjust qoutes

--- hangman.py
+++ hangman.py
@@ -33,19 +33,19 @@ attempt7_string = "  _______\n |       |\n 0       |\n/|\\      |\n |       |\n/
 hangman_graphics = [attempt0_string, attempt1_string, attempt2_string, attempt3_string, attempt4_string, attempt5_string, attempt6_string, attempt7_string]
 
 # Display Hangman welcome message
-for i in range(10) :
+for i in range(10):
     sleep(.3)
     print(i * "*")
 
 print(attempt7_string)
 print("Welcome to Hangman!")
 
-for j in range(10, 0, -1) :
+for j in range(10, 0, -1):
     sleep(.3)
     print(j * "*")
 
 # Game loop  until reaches attempt limit
-while wrong_attempts < 7 :
+while wrong_attempts < 7:
     # turn counter
     turns+=1
     
@@ -57,7 +57,6 @@ while wrong_attempts < 7 :
 
     # Checking the guessed letter if in choosen word
     if guess in choosen_word:
-
         # Notify player if the letter is already been guessed
         if guess in correct_guesses:
             print(f"You already guessed this letter. {guess}")
@@ -65,7 +64,7 @@ while wrong_attempts < 7 :
             # add correct guesses to correct_guesses list
             correct_guesses.append(guess)
             # display hint if user guess is correct
-            display_hint = ''.join(letter if letter in correct_guesses else '_' for letter in choosen_word)
+            display_hint = "".join(letter if letter in correct_guesses else "_" for letter in choosen_word)
 
             print("Correct guess!")
             print(display_hint)
@@ -86,7 +85,8 @@ while wrong_attempts < 7 :
         print(display_hint)
         # display hangman string graphics for wrong attempts
         print(hangman_graphics[wrong_attempts])
-        print("incorrect guesses:",' '.join(wrong_guesses))
+        wrong_guesses_hint = " ".join(wrong_guesses)
+        print(f"incorrect guesses: {wrong_guesses_hint}")
     print("**************************************")
 else:
     # display random phrases for loser
