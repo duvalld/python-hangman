84b878d3e3d2f82f1ba4f126ec0d25340ba829af
Duvall Danganan <duvalldanganan@gmail.com>
2023-11-17 03:07:45 +0800

    "Add basic timer, reafactor code"

--- hangman.py
+++ hangman.py
@@ -1,24 +1,45 @@
 from random import choice
-from time import sleep
+from time import sleep, time
+
 
 # List of words that can be played
 words = ["example", "love", "science", "python", "assignment"]
-
 # List of phrases that can be displayed after you win
 victory_phrase = ["Victory belongs to the most persevering", "You cannot expect victory and plan for defeat"]
-
 # List of phrases that can be displayed after you lose
 death_saying = ["Let the dead bury the dead", "HAHAHAHH! TALO"]
-
-# Choose a word from the list
-choosen_word = choice(words)
 
 # Initialize variables for tracking game state
 correct_guesses = []
 wrong_guesses = []
+game_history = []
 wrong_attempts = 0
 turns = 0
-display_hint = ''
+display_hint = ""
+game_continue = True
+game_counter = 1
+time_limit_per_turn = 30
+continue_counting = True
+game_result = ""
+
+# exit_event = threading.Event()
+# def timeout_handler():
+#     time_count = 0
+#     while time_count < time_limit_per_turn:
+#         sleep(1)
+#         time_count += 1
+#         print(time_count)
+#     print(f"Timeout: {time_limit_per_turn}")
+
+
+def show_hint(correct_list, chooosen_word, wrong_list, wrong_count):
+    # display the correct letter guess
+    print("".join(letter.upper() if letter in correct_list else "_" for letter in chooosen_word))
+    # display the hangman figure
+    print(hangman_graphics[wrong_count])
+    # display the incorrect guesses and number of incorrect guesses
+    wrong_guesses_hint = " ".join(wrong_list)
+    print(f"incorrect guesses: {wrong_guesses_hint.upper()} ({len(wrong_list)})")
 
 # Strings representing hangman visuals for different attempts
 attempt0_string = "  _______\n |       |\n         |\n         |\n         |\n         |\n         |"
@@ -30,69 +51,115 @@ attempt5_string = "  _______\n |       |\n O       |\n/|\\      |\n |       |\n 
 attempt6_string = "  _______\n |       |\n O       |\n/|\\      |\n |       |\n/        |\n         |"
 attempt7_string = "  _______\n |       |\n 0       |\n/|\\      |\n |       |\n/ \\      |\n         |"
 
+# insert the hangman string visuals in a list to manager efficiently
 hangman_graphics = [attempt0_string, attempt1_string, attempt2_string, attempt3_string, attempt4_string, attempt5_string, attempt6_string, attempt7_string]
 
 # Display Hangman welcome message
 for i in range(10):
-    sleep(.3)
+    sleep(.1)
     print(i * "*")
 
 print(attempt7_string)
 print("Welcome to Hangman!")
 
 for j in range(10, 0, -1):
-    sleep(.3)
+    sleep(.1)
     print(j * "*")
 
-# Game loop  until reaches attempt limit
-while wrong_attempts < 7:
-    # turn counter
-    turns+=1
-    
-    # Display current turn number
-    print(f"Turn: {turns}")
+print("Mechanics: For each incorrect guess, a part of a hangman is drawn. The goal is to guess the word before the hangman is fully drawn.")
+# end of the hangman welcome message
 
-    # Input letter
-    guess = input("Guess a letter:")
+# start of the game
+while game_continue:
+    print(f"***********Round {game_counter}**********")
+    # randomly choose word from words list
+    choosen_word = choice(words)
+    print(f"Mystery Word: {len(choosen_word)*"_"}")
+    # Game loop  until reaches attempt limit
+    while wrong_attempts < 7:
+        # turn counter
+        turns+=1
+        # Display current turn number
+        print(f"Turn: {turns}")
 
-    # Checking the guessed letter if in choosen word
-    if guess in choosen_word:
-        # Notify player if the letter is already been guessed
-        if guess in correct_guesses:
-            print(f"You already guessed this letter. {guess}")
-        else:
-            # add correct guesses to correct_guesses list
-            correct_guesses.append(guess)
-            # display hint if user guess is correct
-            display_hint = "".join(letter if letter in correct_guesses else "_" for letter in choosen_word)
-
-            print("Correct guess!")
-            print(display_hint)
-            
-            # display wrong letter 
-            print(hangman_graphics[wrong_attempts])
-            # check if all letters have been guessed
-            if all(letter in correct_guesses for letter in choosen_word):
-                print(choice(victory_phrase))
-                break
-    else: 
-        # append wrong letters to wronge_guesses list
-        wrong_guesses.append(guess)
-        # increase wrong_attempts counter by 1
-        wrong_attempts+=1
         
-        print("Wrong guess!")
-        print(display_hint)
-        # display hangman string graphics for wrong attempts
-        print(hangman_graphics[wrong_attempts])
-        wrong_guesses_hint = " ".join(wrong_guesses)
-        print(f"incorrect guesses: {wrong_guesses_hint}")
-    print("**************************************")
-else:
-    # display random phrases for loser
-    print(choice(death_saying))
-    print("YOU LOSE!")
+    
+        invalid_entry = True
+        # continuously ask user for valid letter (single character alphabet only)
+        timeout_indicator = False
+        start_time = time()
+        while invalid_entry:
+            guess = input("Guess a letter:")
+            if int(time() - start_time) < time_limit_per_turn:
+                if len(guess) == 1:
+                    if guess.isalpha() == False:
+                        print("Invalid input: Please provide alphabetic characters only")
+                    else:
+                        invalid_entry = False
+                else:
+                    print("Invalid Entry: Please provide single character")
+            else:
+                timeout_indicator = True
+                invalid_entry = False
+                print(f"Timeout: You need to guess a letter within {time_limit_per_turn} second(s) {time() - start_time}")
+                
 
-# display the mystery word
-print(f"The word is {choosen_word}")
+        # thread_timeout.join()
+        # convert guess to lowe case
+        guess = guess.lower()
+        # Checking the guessed letter if in choosen word
+        if guess in choosen_word and timeout_indicator == False:
+            # Notify player if the letter is already been guessed
+            if guess in correct_guesses:
+                print(f"You already guessed this letter. {guess}")
+                show_hint(correct_guesses, choosen_word, wrong_guesses, wrong_attempts)
+            else:
+                # add correct guesses to correct_guesses list
+                correct_guesses.append(guess)
+                # display hint if user guess is correct
+                print("Correct guess!")
+                show_hint(correct_guesses, choosen_word, wrong_guesses, wrong_attempts)
 
+                if all(letter in correct_guesses for letter in choosen_word):
+                    print(choice(victory_phrase))
+                    print("YOU WIN!")
+                    game_result = "Win"
+                    break
+        else: 
+            if timeout_indicator == True:
+                wrong_guesses.append("TO")
+            else:
+                # append wrong letters to wronge_guesses list
+                wrong_guesses.append(guess)
+                # wrong guess message
+                print("Wrong guess!")
+            # increase wrong_attempts counter by 1
+            wrong_attempts+=1
+            # display Correct Guesses
+            show_hint(correct_guesses, choosen_word, wrong_guesses, wrong_attempts)
+        # string divider per turn
+        print("**************************************")
+    else:
+        # display random phrases for loser
+        print(choice(death_saying))
+        game_result = "Lose"
+        print("YOU LOSE!")
+
+    # display the mystery word
+    print(f"The word is {choosen_word.upper()}")
+    game_history.append([game_counter, choosen_word, turns, game_result])
+    
+    correct_guesses.clear()
+    wrong_guesses.clear()
+
+    # Ask the player if they want to play again
+    play_again = input("Do you want to play again?(y/n)")
+    if play_again.lower() == "y":
+        game_counter += 1
+    elif play_again.lower() == "n":
+        print("Thank you for Playing!")
+        print("*********Game Results*********")
+        for i_game_counter, i_choosen_word, i_turn, i_game_result in game_history:
+            print(f"Round: {i_game_counter} Word: {i_choosen_word} Turns: {i_turn} Result: {i_game_result}")
+        game_continue = False
+    turns = 0
