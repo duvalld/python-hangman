168f02af9844c1f1287072acce0fe75924935b1d
Duvall Danganan <duvalldanganan@gmail.com>
2023-11-17 10:46:59 +0800

    Include Time Limit per turn function

--- hangman.py
+++ hangman.py
@@ -22,16 +22,7 @@ time_limit_per_turn = 30
 continue_counting = True
 game_result = ""
 
-# exit_event = threading.Event()
-# def timeout_handler():
-#     time_count = 0
-#     while time_count < time_limit_per_turn:
-#         sleep(1)
-#         time_count += 1
-#         print(time_count)
-#     print(f"Timeout: {time_limit_per_turn}")
-
-
+# function for displaying attempts
 def show_hint(correct_list, chooosen_word, wrong_list, wrong_count):
     # display the correct letter guess
     print("".join(letter.upper() if letter in correct_list else "_" for letter in chooosen_word))
@@ -72,43 +63,46 @@ print("Mechanics: For each incorrect guess, a part of a hangman is drawn. The go
 # start of the game
 while game_continue:
     print(f"***********Round {game_counter}**********")
-    # randomly choose word from words list
+    # randomly choose mystery word from words list
     choosen_word = choice(words)
     print(f"Mystery Word: {len(choosen_word)*"_"}")
+    # start of the turn
     # Game loop  until reaches attempt limit
     while wrong_attempts < 7:
         # turn counter
         turns+=1
         # Display current turn number
         print(f"Turn: {turns}")
-
-        
     
         invalid_entry = True
         # continuously ask user for valid letter (single character alphabet only)
         timeout_indicator = False
+
+        # time indicator for turn limit
         start_time = time()
         while invalid_entry:
+            # ask of user input
             guess = input("Guess a letter:")
+            # Time limit condition
             if int(time() - start_time) < time_limit_per_turn:
+                # single character condition
                 if len(guess) == 1:
+                    # accept alphabet only condition
                     if guess.isalpha() == False:
                         print("Invalid input: Please provide alphabetic characters only")
                     else:
+                        # valid Entry
                         invalid_entry = False
                 else:
                     print("Invalid Entry: Please provide single character")
             else:
+                # time limit reached
                 timeout_indicator = True
                 invalid_entry = False
                 print(f"Timeout: You need to guess a letter within {time_limit_per_turn} second(s) {time() - start_time}")
                 
-
-        # thread_timeout.join()
-        # convert guess to lowe case
-        guess = guess.lower()
         # Checking the guessed letter if in choosen word
-        if guess in choosen_word and timeout_indicator == False:
+        if guess.lower() in choosen_word and timeout_indicator == False:
             # Notify player if the letter is already been guessed
             if guess in correct_guesses:
                 print(f"You already guessed this letter. {guess}")
@@ -119,13 +113,15 @@ while game_continue:
                 # display hint if user guess is correct
                 print("Correct guess!")
                 show_hint(correct_guesses, choosen_word, wrong_guesses, wrong_attempts)
-
+                
+                # check if all letter in correct_guesses list match with the mystery word
                 if all(letter in correct_guesses for letter in choosen_word):
                     print(choice(victory_phrase))
                     print("YOU WIN!")
                     game_result = "Win"
                     break
         else: 
+            # wrong attempt condition. if through Time limit or wrong guess
             if timeout_indicator == True:
                 wrong_guesses.append("TO")
             else:
@@ -140,15 +136,17 @@ while game_continue:
         # string divider per turn
         print("**************************************")
     else:
-        # display random phrases for loser
+        # display random phrase for loser
         print(choice(death_saying))
         game_result = "Lose"
         print("YOU LOSE!")
+    # End of turn
 
     # display the mystery word
     print(f"The word is {choosen_word.upper()}")
     game_history.append([game_counter, choosen_word, turns, game_result])
     
+    # reset guess list for next game
     correct_guesses.clear()
     wrong_guesses.clear()
 
@@ -157,9 +155,15 @@ while game_continue:
     if play_again.lower() == "y":
         game_counter += 1
     elif play_again.lower() == "n":
+        # Game over message
+        print("GAME OVER")
         print("Thank you for Playing!")
+        # Display game history
         print("*********Game Results*********")
         for i_game_counter, i_choosen_word, i_turn, i_game_result in game_history:
             print(f"Round: {i_game_counter} Word: {i_choosen_word} Turns: {i_turn} Result: {i_game_result}")
+        # End display game history
+        # change game_continue = False to stop the loop
         game_continue = False
+    # reset the turn counter
     turns = 0
